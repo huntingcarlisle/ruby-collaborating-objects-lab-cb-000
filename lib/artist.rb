5fa7a2f40e93a63bfc27afbd6e8aa99ff2b318b7
Hunter Carlisle <huntingcarlisle@gmail.com>
2018-10-22 23:52:10 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,17 @@
+class Artist
+  attr_accessor :name, :songs
+  @@all
+  def initialize(name)
+    @name = name
+  end
+  
+  def add_song(song)
+    @songs << song
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  
+end
