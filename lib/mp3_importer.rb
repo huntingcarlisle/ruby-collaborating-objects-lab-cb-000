bfae7c1da9c617fda555b402aa703634195904b3
Hunter Carlisle <huntingcarlisle@gmail.com>
2018-10-23 00:31:07 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,12 +1,12 @@
 class MP3Importer
-  attr_accessor :path
+  attr_accessor :path, @files
   
   def initialize(path)
     @path = path
   end
   
   def files
-    
+    @files = Dir.glob("*.jpg")
   end
   
   def import
