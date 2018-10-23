4f825f94d01f66bb7311d7c38f95e1836bf27f7d
Hunter Carlisle <huntingcarlisle@gmail.com>
2018-10-23 00:20:28 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,10 +8,9 @@ class Song
   def self.new_by_filename(filename)
     artist = filename.split(" - ")[0]
     song = filename.split(" - ")[1]
-    genre = filename.split(" - ")[2].split(".")[0]
     new_artist = Artist.find_or_create_by_name(artist)
     new_song = self.new(song)
-    # new_artist.add_song(new_song)
+    new_artist.add_song(new_song)
     
   end
   
